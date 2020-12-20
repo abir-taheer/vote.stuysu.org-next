275278fe86651465b39c7b2fd70ce3a1ed3448a4
Abir Taheer <abir@taheer.me>
2020-12-20 00:42:16 -0500

    added the ability to customize width and height in resource urls

--- graphql/typeDefs/CloudinaryResource.js
+++ graphql/typeDefs/CloudinaryResource.js
@@ -1,13 +1,13 @@
-import {gql} from "apollo-server-micro";
+import { gql } from "apollo-server-micro";
 
 export default gql`
-    type CloudinaryResource {
-        id: String!
-        width: Int!
-        height: Int!
-        format: String!
-        resourceType: String!
-        createdAt: DateTime
-        url: String!
-    }
+  type CloudinaryResource {
+    id: String!
+    width: Int!
+    height: Int!
+    format: String!
+    resourceType: String!
+    createdAt: DateTime
+    url(width: Int, height: Int, quality: Float): String!
+  }
 `;
--- utils/cloudinary/resourceDetailsLoader.js
+++ utils/cloudinary/resourceDetailsLoader.js
@@ -18,6 +18,6 @@ async function batchLoader(publicIds) {
   return publicIds.map(id => publicIdMap[id] || null);
 }
 
-const resourceDetailsLoader = new DataLoader(batchLoader);
+const resourceDetailsLoader = new DataLoader(batchLoader, {cache: false});
 
 export default resourceDetailsLoader;
