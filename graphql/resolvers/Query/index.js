24ca0117a241fc6257ae1e1910aa9c38720c4cae
Abir Taheer <abir@taheer.me>
2020-12-19 23:30:03 -0500

    imported election queries

--- graphql/resolvers/Query/index.js
+++ graphql/resolvers/Query/index.js
@@ -1,7 +1,11 @@
 import userById from "./userById";
+import electionById from "./electionById";
+import electionByUrl from "./electionByUrl";
 
 const Query = {
   userById,
+  electionById,
+  electionByUrl,
 };
 
 export default Query;
