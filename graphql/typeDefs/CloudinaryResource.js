044a4f0f532175722f8eb2ffce2c847113b78917
Abir Taheer <abir@taheer.me>
2020-12-20 00:46:04 -0500

    made resource height and width not required

--- graphql/typeDefs/CloudinaryResource.js
+++ graphql/typeDefs/CloudinaryResource.js
@@ -3,10 +3,10 @@ import { gql } from "apollo-server-micro";
 export default gql`
   type CloudinaryResource {
     id: String!
-    width: Int!
-    height: Int!
-    format: String!
-    resourceType: String!
+    width: Int
+    height: Int
+    format: String
+    resourceType: String
     createdAt: DateTime
     url(width: Int, height: Int, quality: Float): String!
   }
